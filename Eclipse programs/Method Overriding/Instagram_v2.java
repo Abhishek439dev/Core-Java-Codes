8aed1601dba909bf90c9147f1bcce58ed29d76ef
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-27 00:03:35 +0530

    Update Instagram_v2.java

--- Eclipse programs/Method Overriding/Instagram_v2.java
+++ Eclipse programs/Method Overriding/Instagram_v2.java
@@ -1,14 +1,14 @@
 package com;
 
-public class Instagram_v2 extends Instagram_v1{
+public class Instagram_v2 extends Instagram_v1{     //Is-A relationship is necessary for method overriding.
 
 	@Override
-	void upload() {
+	void upload() {     //upload method is overriden 
 		super.upload();
 		System.out.println("You can now upload Images and GIFs as well in the new version.");
 	}
 	
-	void DM() {
+	void DM() {        //A new method 'DM' is created.
 		System.out.println("In this version you can send messages to your friends.");
 	}
 }
