d745b020df200a5deda83c0a9a840a80fafbd7a6
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-07-27 00:05:04 +0530

    Update Instagram_v1.java

--- Eclipse programs/Method Overriding/Instagram_v1.java
+++ Eclipse programs/Method Overriding/Instagram_v1.java
@@ -2,7 +2,7 @@ package com;
 
 public class Instagram_v1 {
 
-	void upload() {
+	void upload() {     // method without overriding.
 		System.out.println("You can upload your images to the app.");
 	}
 }
