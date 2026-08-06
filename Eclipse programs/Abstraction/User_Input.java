abf99950c8fb407a60e6e403b84c668cd9847741
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-06 16:20:28 +0530

    Update User_Input.java

--- Eclipse programs/Abstraction/User_Input.java
+++ Eclipse programs/Abstraction/User_Input.java
@@ -1,28 +1,30 @@
 package com;
 
-import java.util.Scanner;
+import java.util.Scanner;         
 
 public class User_Input {
 	
 	public static void main(String[] args) {
 		
-		Scanner s = new Scanner(System.in);
-		Calculator C = new Logic();
-		
+		Scanner s = new Scanner(System.in);    //Scanner function to take user input.
+		Calculator C = new Logic();            //Upcasting.
+
+		//while loop to repeat the calculator program until exited.
 		while(true) {
 			System.out.println("------------------------");
-			System.out.println("Enter a Choice:\n1)Addition\n2)Substraction\n3)Multiplication\n4)Division\n5)Exit"); 
+			System.out.println("Enter a Choice:\n1)Addition\n2)Substraction\n3)Multiplication\n4)Division\n5)Exit");   //prompt to choose what to do?
 			System.out.println("------------------------");
-			int choice = s.nextInt();
-		
+			int choice = s.nextInt();                              //storing user input in choice variable.
+
+			//switch-case statements to pass user choice and perform actions as per the choice.
 			switch (choice) {
 			case 1: 
-				System.out.println("Enter first number");
-				int num1 = s.nextInt();
-				System.out.println("Enter Second number");
-				int num2 = s.nextInt();
-				System.out.println("Result: "+C.add(num1, num2)); 
-				break;
+				System.out.println("Enter first number");        //prompt to ask for first number.
+				int num1 = s.nextInt();                          //storing first number.
+				System.out.println("Enter Second number");       //prompt to ask for second number.
+				int num2 = s.nextInt();                          //storing second number.
+				System.out.println("Result: "+C.add(num1, num2));  //printing the results along with calling the appropriate method.
+				break;                                              //break statement to come out of the switch-case statements.
 				
 			case 2: 
 				System.out.println("Enter first number");
@@ -49,13 +51,13 @@ public class User_Input {
 				break;
 			
 			case 5: 
-				System.out.println("Thank you for using Calculator App.");
-				C.exit();
-				s.close();
-				break;
+				System.out.println("Thank you for using Calculator App.");     //Last case to exit the ptogram.
+				C.exit();                                                      //Calling the exiting function.
+				s.close();                                                     //closing scanner function.
+				break;                                                         //break statement is necessary to avoid executing the next case statements.
 				
 			default:
-				System.out.println("Invalid Choice");
+				System.out.println("Invalid Choice");                          //default output in case of value input other than the above cases.
 			}
 			
 		}
