da065b3dcf01c134719ccb65bad89d3a1524821d
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-06 16:12:02 +0530

    Update Logic.java

--- Eclipse programs/Abstraction/Logic.java
+++ Eclipse programs/Abstraction/Logic.java
@@ -1,19 +1,26 @@
 package com;
 
+//Is-A relationship from class to interface using implements keyword.
 public class Logic implements Calculator {
-	
+
+	//overriding the methods inherited from the Calculator interface by providing them an implementation as per their function.
+	@override
 	public int add(int a, int b) {
 		return a+b;
 	}
+	@override
 	public int sub(int a, int b) {
 		return a-b;
 	}
+	@override
 	public int multiply(int a, int b) {
 		return a*b;
 	}
+	@override
 	public int divide(int a, int b) {
 		return a/b;
 	}
+	@override
 	public void exit() {
 		System.exit(0);
 	}
