fd06ca9922045257c579948cf02266424f880f09
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-06 16:25:28 +0530

    Update Calculator.java

--- Eclipse programs/Abstraction/Calculator.java
+++ Eclipse programs/Abstraction/Calculator.java
@@ -5,7 +5,7 @@ public interface Calculator {
 
 	//interface only stores public and abstract methods(no need to declare them as public and abstract) by default.
 	int add(int a, int b);                  //addition method.
-	int sub(int a, int b);                  //substraction method.
+	int sub(int a, int b);                  //subtraction method.
 	int multiply(int a, int b);             //multiplication method.
 	int divide(int a, int b);               //division method.
 	void exit();                            //exit method.
