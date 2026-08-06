ff7de49d212523f253385f289d58e142993f8343
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-06 16:24:45 +0530

    Update Logic.java

--- Eclipse programs/Abstraction/Logic.java
+++ Eclipse programs/Abstraction/Logic.java
@@ -4,23 +4,23 @@ package com;
 public class Logic implements Calculator {
 
 	//overriding the methods inherited from the Calculator interface by providing them an implementation as per their function.
-	@override
+	@Override
 	public int add(int a, int b) {
 		return a+b;
 	}
-	@override
+	@Override
 	public int sub(int a, int b) {
 		return a-b;
 	}
-	@override
+	@Override
 	public int multiply(int a, int b) {
 		return a*b;
 	}
-	@override
+	@Override
 	public int divide(int a, int b) {
 		return a/b;
 	}
-	@override
+	@Override
 	public void exit() {
 		System.exit(0);   //This command is used to terminate the program. 0 -> successful termination, 1 -> runtime exception, 2-> syntax error, -1 -> external error
 	}
