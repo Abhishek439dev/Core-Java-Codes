4cfa9408dd938003b8853a239632c000ccf5a1c0
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-07 23:39:54 +0530

    Update Son.java

--- Eclipse programs/Abstract and Concrete/Constructors in abstract class/Son.java
+++ Eclipse programs/Abstract and Concrete/Constructors in abstract class/Son.java
@@ -1,19 +1,18 @@
 package org;
 
+//IS-A relationship
 public class Son extends Father{
 
-	int sAge;
-	String sName;
-	
+	//subclass constructor
 	Son(){
-		super(50, "JM Saxena");
+		super(50, "JM Saxena");                                //super calling statement used explicitly to call the superclass const. 
 		System.out.println("This is Son's constructor");
 	}
 	
 	public static void main(String[] args) {
 		
-		Son s = new Son();
-		System.out.println("Father's age is "+s.fAge+" and his name is "+s.fName);
+		Son s = new Son();                                     //object creation of concrete subclass to invoke its constrcutor. 
+		System.out.println("Father's age is "+s.fAge+" and his name is "+s.fName);  //Accessing variables initialized by superclass const.
 	}
 }
 
