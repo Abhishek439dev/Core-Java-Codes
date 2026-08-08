fbb4029d6ec3b7c61d879a0b44934641b6408e65
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-08 17:53:58 +0530

    Update Employee.java

--- Eclipse programs/Interface/Employee.java
+++ Eclipse programs/Interface/Employee.java
@@ -1,16 +1,16 @@
 package com;
 
-public class Employee implements Person{
+public class Employee implements Person{                                    //class --> interface Is-A relationship is achieved using 'implements' keyword.
 
 	@Override
-	public void Work() {
+	public void Work() {                                                     //The abstract method declared in 'Person' interface needs to be override.
 		System.out.println("This person is an employee of XYZ company.");
 	}
 	
 	public static void main(String[] args) {
-		Employee e  = new Employee();
-		e.Work();
+		Employee e  = new Employee();                                       //Object of the sub-class.
+		e.Work();                                                           //overrided method called.
 		
-		System.out.println("The age of this employee is "+Person.age);
+		System.out.println("The age of this employee is "+Person.age);      //Variables in interface are static hence, className.variableName
 	}
 }
