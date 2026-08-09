35e4c41296a5f5999baced097aaeadfa1327a861
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-09 13:16:33 +0530

    Update Bank.java

--- Eclipse programs/Abstraction/Bank App program/Bank.java
+++ Eclipse programs/Abstraction/Bank App program/Bank.java
@@ -1,10 +1,11 @@
 package org;
 
+//Interface used to show what the application can do. 
 public interface Bank {
 	
-	void Deposit(int dAmt);
-	void Withdraw(int wAmt);
-	void Balance();
-	void Exit();
+	void Deposit(int dAmt);    //Amount depositing method.
+	void Withdraw(int wAmt);   //withdrawing method.
+	void Balance();            //balance checking method.
+	void Exit();               //Method to close the program.
 
 }
