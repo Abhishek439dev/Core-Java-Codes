6cfd8b4f746ef251ec839cc53da23c4fef2838b0
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-09 13:32:20 +0530

    Update UserAccount.java

--- Eclipse programs/Abstraction/Bank App program/UserAccount.java
+++ Eclipse programs/Abstraction/Bank App program/UserAccount.java
@@ -5,38 +5,44 @@ import java.util.Scanner;
 public class UserAccount {
 
 	public static void main(String[] args) {
-		Bank B = new ATM();
-		Scanner scan = new Scanner(System.in);
+		Bank B = new ATM();                             //Bank class instantiased to access all the methods.
+		Scanner scan = new Scanner(System.in);          //Scanner object creation to accept user inputs.
 		
-		while(true) {
+		while(true) {                                   //while loop to repeat the process until exited via exiting method.
 			System.out.println("-------------------------------");
-			System.out.println("Welcome to XYZ Bank App\nPlease choose from the menu.\n1)Deposit\n2)Withdraw\n3)Check Balance\n4)Exit");
+			System.out.println("Welcome to XYZ Bank App\nPlease choose from the menu.\n1)Deposit\n2)Withdraw\n3)Check Balance\n4)Exit");  //Prompt to ask user input.
 			System.out.println("-------------------------------");
-			int choice = scan.nextInt();
-			switch(choice) {
+			int choice = scan.nextInt();                //storing user input in choice.
 			
+			//switch statements to perform action as per user input.
+			switch(choice) {
+
+			//for Depositing
 			case 1: 
 				System.out.println("Enter the amount you want to deposit");
-				int dAmt = scan.nextInt();
-				B.Deposit(dAmt);
-				break;
-				
+				int dAmt = scan.nextInt();   //asking the deposit amount.
+				B.Deposit(dAmt);             //Calling the deposit function written in Bank class and passing the amount entered by user.
+				break;                       //to stop execution of other case statements.
+
+			//for Withdrawl
 			case 2:
 				System.out.println("Enter the amount you want to withdraw.");
-				int wAmt = scan.nextInt();
-				B.Withdraw(wAmt);
+				int wAmt = scan.nextInt();   //asking the withdrawl amount.
+				B.Withdraw(wAmt);            //Calling the withdrawl function written in Bank class and passing the amount entered by user.
 				break;
-			
+
+			//to check balance
 			case 3: 
-				B.Balance();
+				B.Balance();                 //Calling the balance checking method from Bank class.
 				break;
-				
+
+			//to close the program
 			case 4: 
-				B.Exit();
-				scan.close();
+				B.Exit();                    //Calling the exit method.
+				scan.close();                //Stop accepting user input.
 				
-			default:
-				System.out.println("Please enter a valid choice");
+			default:           
+				System.out.println("Please enter a valid choice");  //default case if user enters an invalid choice.
 			
 			}
 		}
