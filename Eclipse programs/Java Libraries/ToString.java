c65deba3a1c559ba61c37ebe3e466b7f97b23867
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-15 23:55:55 +0530

    Update ToString.java

--- Eclipse programs/Java Libraries/ToString.java
+++ Eclipse programs/Java Libraries/ToString.java
@@ -1,10 +1,17 @@
 package object;
-
+//This is a method of Object class which returns the String representation of an object. 
 public class ToString {
 
 	public static void main(String[] args) {
-		ToString Ts = new ToString();
-		System.out.println(Ts);  //String Representation
-		System.out.println(Ts.toString());
+		
+		ToString Ts = new ToString(); // An Object of class is created.
+		System.out.println(Ts);  //String Representation = Fully Qualified Class Name@ Hexadecimal value of HashCode.
+		System.out.println(Ts.toString());  //Explicitly called.
 	}
 }
+
+/*
+Result:
+object.ToString@65b3120a
+object.ToString@65b3120a
+*/
