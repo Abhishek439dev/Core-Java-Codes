628e94dd602902b59a207fb86ac2b13e18741c6e
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-16 18:15:04 +0530

    Update Equals.java

--- Eclipse programs/Java Libraries/ObjectClassMethods/Equals.java
+++ Eclipse programs/Java Libraries/ObjectClassMethods/Equals.java
@@ -1,11 +1,13 @@
 package object;
-
+//This method returns a boolean value after comparing the hascode values of the two objects.
 public class Equals {
 
 	public static void main(String[] args) {
 		Equals e1 = new Equals();
 		Equals e2 = new Equals();
-		
+
+		//it can be called in two ways i) by using == equal operator ii) by using the equals method calling statement.
+		//hashcode of no two objects can be same.
 		System.out.println(e1 == e2);       //false
 		System.out.println(e1.equals(e2));  //false
 	}
