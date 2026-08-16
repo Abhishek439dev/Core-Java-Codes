251ec7740122ac43db790db2b718aeadda3d6a7b
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-16 18:07:12 +0530

    Add files via upload

---
+++ Eclipse programs/Java Libraries/ObjectClassMethods/ToString.java
@@ -0,0 +1,11 @@
+package object;
+//This is a method of Object class which returns the String representation of an object. 
+public class ToString {
+
+	public static void main(String[] args) {
+		
+		ToString Ts = new ToString(); // An Object of class is created.
+		System.out.println(Ts);  //String Representation = Fully Qualified Class Name@ Hexadecimal value of HashCode.
+		System.out.println(Ts.toString());  //Explicitly called.
+	}
+}
---
+++ Eclipse programs/Java Libraries/ObjectClassMethods/ToStringOverride.java
@@ -0,0 +1,26 @@
+package object;
+
+public class ToStringOverride {
+	String name;  //Member variable declared.
+
+	//Constructor declared.
+	public ToStringOverride(String name) {
+		this.name = name;
+	}
+
+	//toString method is overridden to return the value which is passed to the constructor while object creation.
+	@Override
+	public String toString() {
+		return name;   //or this.name;
+	}
+	
+	public static void main(String[] args) {
+		//Objects created and value is passed as constructor is accepting String values.
+		ToStringOverride TSO1 = new ToStringOverride("Tom");
+		ToStringOverride TSO2 = new ToStringOverride("Jerry");
+		
+		//the object references are printed.
+		System.out.println(TSO1);
+		System.out.println(TSO2);
+	}
+}
