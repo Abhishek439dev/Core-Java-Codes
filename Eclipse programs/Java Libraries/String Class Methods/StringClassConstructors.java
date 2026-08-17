77b60fe78ac7b22f287d01fde00b38f23f237a62
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-17 17:31:36 +0530

    Update StringClassConstructors.java

--- Eclipse programs/Java Libraries/String Class Methods/StringClassConstructors.java
+++ Eclipse programs/Java Libraries/String Class Methods/StringClassConstructors.java
@@ -4,16 +4,18 @@ public class StringClassConstructors {
 
 	public static void main(String[] args) {
 		
-		
-		String s1 = new String("Hello there!");
-		System.out.println(s1);
-		
-		String s = new String("");
-		System.out.println(s);
-		
-		char[] a = {'A','b','h','i','s','h','e','k'};
-		String s2 = new String(a);
-		System.out.println(s2);
+		//Passing set of characters to the String.
+		String s = new String("Hello there!");
+		System.out.println(s);        //Hello there!
+
+		//Empty representation of a String object. Its equivalent to declaring an empty String. (String name = "";)
+		String s1 = new String("");
+		System.out.println(s1);       //blank output.
+
+		//Passing a char[] array to the String object which converts into a String.
+		char[] a = {'A','b','h','i','s','h','e','k'};  //array
+		String s2 = new String(a);    //passing the array as String object
+		System.out.println(s2);       //Abhishek
 		
 		}
 		
