6d14ee2f72c12ca6717ac13253715a9464a9c326
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-20 13:45:27 +0530

    Update PrimeMinister.java

--- Eclipse programs/Java Libraries/Singleton Class/PrimeMinister.java
+++ Eclipse programs/Java Libraries/Singleton Class/PrimeMinister.java
@@ -2,21 +2,24 @@ package singleton;
 
 public class PrimeMinister {
 
-	private static PrimeMinister PM;
-	String PMname = "Rahul Gandhi";
-	
+	private static PrimeMinister PM;  //private static non-primitive variable to store the class object.
+	String PMname = "Rahul Gandhi";  //
+
+	//Private constructor: Prevents object creation in another class.
 	private PrimeMinister() {
-		System.out.println("Prime Minister got elected");
+		System.out.println("Prime Minister got elected");  //message to print when invoked.
 	}
-	
+
+	//public static helper method
 	public static PrimeMinister electPM() {
+		//condition to check if the refernce variable is storing an object already.
 		if(PM == null) {
-			PM = new PrimeMinister();
+			PM = new PrimeMinister();   //if not create an object.
 		}
 		else {
-			System.out.println("Prime Minister already elected.");
+			System.out.println("Prime Minister already elected.");  //if yes, object creation is not allowed.
 		}
 		
-		return PM;
+		return PM;  //returning the reference which is storing the object, so that we can access the variables of this class in another class.
 	}
 }
