dfce9745bc61db8e9aba04e5906c5cd1b9abe1f4
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-21 16:50:36 +0530

    Update TryCatch3.java

--- Eclipse programs/Exception Handling/Try-Catch/TryCatch3.java
+++ Eclipse programs/Exception Handling/Try-Catch/TryCatch3.java
@@ -17,15 +17,15 @@ public class TryCatch3 {
 		try {
 			System.out.println("The Quotient is: "+num1/num2);
 		}
-			//catch block one catching a NullPointerException if occured.
+			//catch block one catching a NullPointerException if occurred.
 		catch(NullPointerException e) {
 			System.out.println("Invalid");
 		}
-			//catch block two catching a ArrayIndexOutOfBoundsException if occured.
+			//catch block two catching a ArrayIndexOutOfBoundsException if occurred.
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid index");
 		}
-			//catch block third will catch all the exceptions as Exeception is the Superclass of all Exceptions. So, it can be used if we don't know the exception name.
+			//catch block third will catch all the exceptions as Exception is the Superclass of all Exceptions. So, it can be used if we don't know the exception name.
 			//the super class exception catch block should be written at last, otherwise the other catch blocks will become unreachable.
 		catch(Exception e) {
 			System.out.println("[INVALID] SuperClass Exception Handler");
