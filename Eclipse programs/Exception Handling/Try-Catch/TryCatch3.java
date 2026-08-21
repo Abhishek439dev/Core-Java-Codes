184fe3bda4fb1642b5ad6b97f816df4f9171a686
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-21 16:46:00 +0530

    Update TryCatch3.java

--- Eclipse programs/Exception Handling/Try-Catch/TryCatch3.java
+++ Eclipse programs/Exception Handling/Try-Catch/TryCatch3.java
@@ -3,26 +3,30 @@ package com;
 import java.util.Scanner;
 
 public class TryCatch3 {
-
+//In this program we have multiple catch blocks in case we don't know what kind of exception might occur. 
 	public static void main(String[] args) {
 		
 		System.out.println("<------Welcome to division calculator----->");
-		
+		//Accepting dynamic user inputs to calculate.
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the numerator");
 		int num1 = scan.nextInt();
 		System.out.println("Enter the denominator");
 		int num2 = scan.nextInt();
-		
+		//critical code under try block.
 		try {
 			System.out.println("The Quotient is: "+num1/num2);
 		}
+			//catch block one catching a NullPointerException if occured.
 		catch(NullPointerException e) {
 			System.out.println("Invalid");
 		}
+			//catch block two catching a ArrayIndexOutOfBoundsException if occured.
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid index");
 		}
+			//catch block third will catch all the exceptions as Exeception is the Superclass of all Exceptions. So, it can be used if we don't know the exception name.
+			//the super class exception catch block should be written at last, otherwise the other catch blocks will become unreachable.
 		catch(Exception e) {
 			System.out.println("[INVALID] SuperClass Exception Handler");
 		}
