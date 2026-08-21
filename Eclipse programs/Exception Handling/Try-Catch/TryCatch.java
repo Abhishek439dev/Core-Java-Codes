52ec148d5e5d80ed56978c8a3a4fcc457bdacef1
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-21 16:34:36 +0530

    Update TryCatch.java

--- Eclipse programs/Exception Handling/Try-Catch/TryCatch.java
+++ Eclipse programs/Exception Handling/Try-Catch/TryCatch.java
@@ -3,11 +3,11 @@ package com;
 import java.util.Scanner;
 
 public class TryCatch {
-
+//This program demonstrates the functionality of try and catch statements.
 	public static void main(String[] args) {
 		
 		System.out.println("<------Welcome to division calculator----->");
-		
+		//Accepting dynamic inputs from user.
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the numerator");
 		int num1 = scan.nextInt();
@@ -15,13 +15,14 @@ public class TryCatch {
 		int num2 = scan.nextInt();
 		
 		try {
-			System.out.println("The Quotient is: "+num1/num2);
+			System.out.println("The Quotient is: "+num1/num2);   //Critical lines of code should be there in try block.
 		}
+		//As we knew an Arithmetic exception can occur we can directly write the exception name. variable e is storing the object thrown by ArithmeticException class.
 		catch(ArithmeticException e) {
-			System.out.println("Invalid denominator");
+			System.out.println("Invalid denominator");  //catch block contains the code that should execute once exception occurs.
 		}
 			
-		System.out.println("<-----Thanks for using---->");
+		System.out.println("<-----Thanks for using---->");  //This line of code won't execute if exception occurs and is not handled.
 		scan.close();
 		
 	}
