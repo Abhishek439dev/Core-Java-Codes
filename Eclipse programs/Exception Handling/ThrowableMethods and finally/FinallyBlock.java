00bfb368e1e04b2922b8599e25285e7451398cb0
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-22 13:35:59 +0530

    Update FinallyBlock.java

--- Eclipse programs/Exception Handling/ThrowableMethods and finally/FinallyBlock.java
+++ Eclipse programs/Exception Handling/ThrowableMethods and finally/FinallyBlock.java
@@ -6,25 +6,26 @@ public class FinallyBlock {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter numerator");
-		int num1 = scan.nextInt();
+		Scanner scan = new Scanner(System.in);  //accepting dynamic inputs from user.
+		System.out.println("Enter numerator");  //prompt
+		int num1 = scan.nextInt();              //storing the user input in variable num1.
 		
-		System.out.println("Enter denominator");
-		int num2 = scan.nextInt();
+		System.out.println("Enter denominator"); //prompt
+		int num2 = scan.nextInt();              //storing the user input in variable num2.
 		
 		try {
-			System.out.println(num1/num2);
+			System.out.println(num1/num2);      //Storing the critical code in try block.
 		}
 		catch(ArithmeticException e) {
-			System.out.println("There is some problem ->"+e.getMessage());
+			System.out.println("There is some problem ->"+e.getMessage());   //Execption handled and the small message for the exception occurence is printed.
 		}
-		
+
+		//This block of code will execute always no matter exception occurs or not and if it is handled properly or not.
 		finally {
-			System.out.println("Thank you for using our service.");
+			System.out.println("Thank you for using our service.");  
 		}
 		
-		System.out.println("End");
+		System.out.println("End"); //This line won't execute if an exception occured but is not handled with a suitable exception class.
 		scan.close();
 	}
 }
