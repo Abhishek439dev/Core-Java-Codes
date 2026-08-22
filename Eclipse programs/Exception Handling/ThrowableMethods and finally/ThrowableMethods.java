11b2250d070c0e4d8f901552663a4a797e9c092b
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-22 13:30:57 +0530

    Update ThrowableMethods.java

--- Eclipse programs/Exception Handling/ThrowableMethods and finally/ThrowableMethods.java
+++ Eclipse programs/Exception Handling/ThrowableMethods and finally/ThrowableMethods.java
@@ -4,16 +4,27 @@ public class ThrowableMethods {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Start");
+		System.out.println("Start");  //Marks the begining of the program.
 		
 		try {
-			System.out.println(10/0);
+			System.out.println(10/0);  //Error prone code written in try block.
 		}
-		catch(Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
+		catch(Exception e) {          
+			e.printStackTrace();       //Will print a detailed explanation of the exception.
+			System.out.println(e.getMessage());    //will print a small message describing the reason of exception.
 		}
 		
-		System.out.println("End");
+		System.out.println("End");  //Marks the end of the program.
 	}
 }
+
+
+/*
+Output: 
+Start
+java.lang.ArithmeticException: / by zero
+	at Exception_Handling/org.ThrowableMethods.main(ThrowableMethods.java:10)
+/ by zero
+End
+
+*/
