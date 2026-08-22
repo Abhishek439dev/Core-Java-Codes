2e71370643206fc2b3b3d762a1db3f25b9f46e81
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-22 13:26:42 +0530

    Add files via upload

---
+++ Eclipse programs/Exception Handling/ThrowableMethods and finally/ThrowableMethods.java
@@ -0,0 +1,19 @@
+package org;
+
+public class ThrowableMethods {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start");
+		
+		try {
+			System.out.println(10/0);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("End");
+	}
+}
