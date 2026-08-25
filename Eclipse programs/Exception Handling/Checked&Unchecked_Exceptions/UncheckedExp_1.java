02811920c151243f41c5da4f03a97ddb52f8f021
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-25 16:26:06 +0530

    Update UncheckedExp_1.java

--- Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/UncheckedExp_1.java
+++ Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/UncheckedExp_1.java
@@ -1,13 +1,15 @@
 package in;
 
 public class UncheckedExp_1 {
-//In this program we have shown an Unchecked exception and its not handled to show that the compiler is not giving us any problem.
+//In this program we have some lines of code which can give an Unchecked exception and its not handled to show that the compiler is not giving us any problem.
 	public static void main(String[] args) {
 		
-		int[] arr = {1,2,3,4,5};
+		//An array with 5 elements.
+		int[] arr = {1,2,3,4,5};  
 		
-		System.out.println(arr[90]);  //Accessing an element on index 90. This line can result in ArrayIndexOutOfBoundsException but it will not give us an immediate warning.
-		//We have to handle it using try-catch blocks obviously to ensure smooth execution of program but compiler is not giving us any redline indication.
+		System.out.println(arr[90]);  //Accessing an element on index 90. This line will result in ArrayIndexOutOfBoundsException but it will not give us an immediate warning.
+		//We have to handle it using try-catch blocks obviously to ensure smooth execution of program but compiler is not giving us any redline indication. 
+		//The code will compile succefully.
 	}
 }
 
