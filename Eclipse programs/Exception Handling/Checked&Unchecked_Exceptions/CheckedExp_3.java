83bcce7e367837d574f906dd0a23c5d27e360b5e
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-26 23:20:40 +0530

    Update CheckedExp_3.java

--- Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/CheckedExp_3.java
+++ Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/CheckedExp_3.java
@@ -7,7 +7,7 @@ public class CheckedExp_3 {
 	public static void main(String[] args) {
 		
 		try {
-			FileReader f = new FileReader("demo.txt");   //this line of code can give a FileNotFoundException, if the given file is not present. If we don't handle it, the compiler will show redline under the code.
+			FileReader f = new FileReader("demo.txt");   //this line of code can give a "FileNotFoundException", if the given file is not present. If we don't handle it, the compiler will show redline under the code.
 		}
 		catch(Exception e) {
 			System.out.println("file not found");
