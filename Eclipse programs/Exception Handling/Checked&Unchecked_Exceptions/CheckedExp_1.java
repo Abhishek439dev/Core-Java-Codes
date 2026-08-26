b9f260b2ef73d940656034361ead5f15749f39d2
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-26 23:23:27 +0530

    Update CheckedExp_1.java

--- Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/CheckedExp_1.java
+++ Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/CheckedExp_1.java
@@ -10,7 +10,7 @@ public class CheckedExp_1 {
 			
 	//This line can give InterruptedException and the compiler is letting us know by indicating a redline below the code line. This line of code
 	//will not be compiled until it is handled via try-catch.
-			Thread.sleep(2000);  //This function prints the next number with a delay of 2 seconds.
+			Thread.sleep(2000);  //This function prints the next number with a delay of 2 seconds(in an IDE this line will have redline under it).
 		}
 	}
 }
