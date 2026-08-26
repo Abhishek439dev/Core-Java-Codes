97bdf320318d4c9fd7cf346469250e55334c3f62
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-26 23:19:47 +0530

    Update CheckedExp_2.java

--- Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/CheckedExp_2.java
+++ Eclipse programs/Exception Handling/Checked&Unchecked_Exceptions/CheckedExp_2.java
@@ -3,13 +3,13 @@ package in;
 public class CheckedExp_2 {
 //This program shows a checked exception and we have handled it as it should be using the try-catch blocks to ensure proper execution of program.
 	public static void main(String[] args) {
-		
+		//Printing numbers from 1 to 5.
 		for(int i = 1; i <= 5; i++) {
 			System.out.println(i);
 			
 			//Handling the critical code using try-catch.
 			try {
-				Thread.sleep(2000); // this method prints the next iteration with a delay of 2 seconds.
+				Thread.sleep(2000); // this method prints the next iteration with a delay of 2 seconds. (We can't run the program without handling this line.)
 			}
 			catch(Exception e) {
 				e.printStackTrace();
