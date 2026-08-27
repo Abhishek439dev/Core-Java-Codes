8771679cc4bfbea83a6d717148e5c3e9dcf04886
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-27 23:53:31 +0530

    Update WithdrawAmount.java

--- Eclipse programs/Exception Handling/Custom Exceptions/WithdrawAmount.java
+++ Eclipse programs/Exception Handling/Custom Exceptions/WithdrawAmount.java
@@ -6,21 +6,22 @@ public class WithdrawAmount {
 
 	public static void main(String[] args) {
 		
-		int balance = 10000;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the amount to withdraw");
-		int amt = scan.nextInt();
-		scan.close();
-		
+		int balance = 10000;                                    //initial balance.
+		Scanner scan = new Scanner(System.in);                  //Acceepting dynamic inputs.
+		System.out.println("Enter the amount to withdraw");     //Prompt
+		int amt = scan.nextInt();                               //User input stored in variable.
+		scan.close();                                           //Scanner method closed.
+
+		//Conditoinal statements to define when to invoke the exception.
 		if(amt <= balance) {
 			System.out.println(amt+" withdrawn successfully");
 		}
-		else {
+		else {      //throw exception when withdrawing amount is more than the available balance and we have to handle it immediately(checked exception).
 			try {
-				throw new InsufficientBalanceException("Insufficient balance");
+				throw new InsufficientBalanceException("Insufficient balance");   //throw keyword is used to throw the exception object and we have given the String input which will initialize the private variable.
 			}
 			catch(InsufficientBalanceException e) {
-				System.out.println(e.getMessage());
+				System.out.println(e.getMessage());  //Printing the short message of exception which was initialized above by throwing class object.
 			}
 			
 		}
