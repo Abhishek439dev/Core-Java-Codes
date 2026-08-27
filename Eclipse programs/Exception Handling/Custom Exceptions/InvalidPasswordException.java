b454141d01ad451604816fb705480c51931512f3
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-27 23:55:01 +0530

    Update InvalidPasswordException.java

--- Eclipse programs/Exception Handling/Custom Exceptions/InvalidPasswordException.java
+++ Eclipse programs/Exception Handling/Custom Exceptions/InvalidPasswordException.java
@@ -1,11 +1,11 @@
 package customExp;
-
+//Created a class which is inheriting the RuntimeException class due to which it is an Unchecked exception.
 public class InvalidPasswordException extends RuntimeException{
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;   //SerialId of custom exception class.
 
 	
 }
