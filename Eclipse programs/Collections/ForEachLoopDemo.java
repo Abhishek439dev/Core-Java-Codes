3191776157d367eecd2f778cc8d93cbd00e23b88
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-15 23:08:06 +0530

    Update ForEachLoopDemo.java

--- Eclipse programs/Collections/ForEachLoopDemo.java
+++ Eclipse programs/Collections/ForEachLoopDemo.java
@@ -1,14 +1,14 @@
 package list;
-
+//forEach loop or enhanced for loop is a more efficient way to traverse through a group of objects like array or collections.
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ForEachLoopDemo {
 
 	public static void main(String[] args) {
-		
+		//Using for each loop on array.
 		int[] a = {10,20,30,40,50};
-		
+		//Syntax: for(common-datatype-for-all-objects reference-variable  :  reference-variable of the array/collection)
 		for(int i : a) {
 			System.out.println(i);
 		}
@@ -22,9 +22,9 @@ public class ForEachLoopDemo {
 		}
 		
 		System.out.println("---------------");
-		
+		//Using for each loop in collections.
 		ArrayList<Integer> Al = new ArrayList<Integer>();
-		
+		//Adding objects in the ArrayList.
 		Al.add(10);
 		Al.add(20);
 		Al.add(30);
@@ -44,7 +44,7 @@ public class ForEachLoopDemo {
 		ll.add(null);
 		ll.add(30.45);
 		ll.add("Apple");
-		
+		//As there are heterogenous objects, we can use Object class as a non-primitive data type because it is inherited by all the classes(wrapper classes as well).
 		for(Object obj : ll) {
 			System.out.println(obj);
 		}
