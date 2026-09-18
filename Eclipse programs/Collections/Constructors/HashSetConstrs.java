2856dcd5e6f2ba0e112e405a99dfa446a4e5bf95
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-18 23:28:23 +0530

    Update HashSetConstrs.java

--- Eclipse programs/Collections/Constructors/HashSetConstrs.java
+++ Eclipse programs/Collections/Constructors/HashSetConstrs.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class HashSetConstrs {
 
 	public static void main(String[] args) {
+		//i) HashSet() -> used to create a default HashSet of capacity 16.
 		HashSet<Object> hs = new HashSet<Object>();
 	
 		hs.add("Java");
@@ -14,7 +15,8 @@ public class HashSetConstrs {
 		hs.add('S');
 		
 		System.out.println(hs);
-		
+
+		//ii) HashSet(int initialCapacity) -> this one is used to create a HashSet with custom initial capacity.
 		HashSet<Object> hs2 = new HashSet<Object>(10);
 		hs2.add("Java");
 		hs2.add(10);
@@ -30,7 +32,8 @@ public class HashSetConstrs {
 		hs2.add("Programming");
 		
 		System.out.println(hs2);
-		
+
+		//iii) HashSet(int initialCapacity, double load factor) -> This is used to create a HashSet with custom capacity and custom load factor.
 		HashSet<Object> hs3 = new HashSet<Object>(10, 0.75f);
 		hs3.add("Java");
 		hs3.add(10);
@@ -46,7 +49,8 @@ public class HashSetConstrs {
 		hs3.add("Programming");
 	
 		System.out.println(hs3);
-		
+
+		//iv) HashSet(Collection c) -> this is used to create a HashSet which obtains its values from another collection in one go.
 		HashSet<Object> hs4 = new HashSet<Object>(hs);
 		System.out.println(hs4);
 		
