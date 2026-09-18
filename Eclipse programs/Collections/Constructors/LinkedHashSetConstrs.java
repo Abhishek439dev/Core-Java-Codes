ca2caf60118da0d507e02a7dbdc370ae20104896
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-18 23:33:21 +0530

    Update LinkedHashSetConstrs.java

--- Eclipse programs/Collections/Constructors/LinkedHashSetConstrs.java
+++ Eclipse programs/Collections/Constructors/LinkedHashSetConstrs.java
@@ -1,11 +1,12 @@
 package constructors;
 
 import java.util.LinkedHashSet;
-
+//Constructors of LinkedHashSet() are similar to that of HashSet()
 public class LinkedHashSetConstrs {
 
 	public static void main(String[] args) {
-		
+
+		//i) LinkedHashSet() -> Used to create a LinkedHashSet of default capacity of 16.
 		LinkedHashSet<Object> lhs = new LinkedHashSet<Object>();
 		
 		lhs.add("Java");
@@ -15,7 +16,8 @@ public class LinkedHashSetConstrs {
 		lhs.add('S');
 		
 		System.out.println(lhs);
-		
+
+		//ii) LinkedHashSet(int initialCapacity) -> Used to create a LinkedHashSet of custom capacity.
 		LinkedHashSet<Object> lhs2 = new LinkedHashSet<Object>(10);
 		lhs2.add("Java");
 		lhs2.add(10);
@@ -31,7 +33,8 @@ public class LinkedHashSetConstrs {
 		lhs2.add("Programming");
 		
 		System.out.println(lhs2);
-		
+
+		//iii) LinkedHashSet(int initialCapacity, float loadFactor) -> Used to create a LinkedHashSet of custom capacity and load factor.
 		LinkedHashSet<Object> lhs3 = new LinkedHashSet<Object>(10, 0.75f);
 		lhs3.add("Java");
 		lhs3.add(10);
@@ -47,7 +50,8 @@ public class LinkedHashSetConstrs {
 		lhs3.add("Programming");
 		
 		System.out.println(lhs3);
-		
+
+		//iv) LinkedHashSet(Collection c) -> Used to create a LinkedHashSet which obtains its values from another collection.
 		LinkedHashSet<Object> lhs4 = new LinkedHashSet<Object>(lhs);
 		System.out.println(lhs4);
 	}
