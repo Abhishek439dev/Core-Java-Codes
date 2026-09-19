a97462841dfa52f949fb8e6e592c6e64969814ed
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-19 13:20:20 +0530

    Update HashSetDemo.java

--- Eclipse programs/Collections/Set/HashSetDemo.java
+++ Eclipse programs/Collections/Set/HashSetDemo.java
@@ -4,10 +4,10 @@ import java.util.HashSet;  //HashSet class imported to use it.
 public class HashSetDemo {
 
 	public static void main(String[] args) {
-		//Creating a HashSet
+		//Creating a HashSet to store the Employee objects.
 		HashSet<Employee> hs = new HashSet<Employee>();
 
-		//Creating Employee class objects to invoke the constructor and intialize the variables. 
+		//Creating Employee class objects to invoke the constructor and initialize the variables. 
 		Employee e1 = new Employee(101, 24, "Eren Yeagr", 12000);
 		Employee e2 = new Employee(102, 25, "Levi Ackerman", 17000);
 		Employee e3 = new Employee(103, 22, "Arvin Smith", 20000);
@@ -22,17 +22,38 @@ public class HashSetDemo {
 		hs.add(e5);
 		hs.add(new Employee(106, 24, "Armin Arlet", 12000));
 		
-		//null String values -> allowed
-		hs.add(new Employee(107, 24, null, 14000));
+		//Objects with null values allowed because these are separate different objects in a set, their values are not checked, reference is checked for duplicacy.
+		//They may look identical as their values are same (null) but these are two different objects, that's why they are allowed in set.
+		hs.add(new Employee(null, null, null, null));
+		hs.add(new Employee(null, null, null, null));
 		
-		//Duplicate objects -> Not Allowed
+		//Duplicate objects -> Not Allowed because these reference have already added above in the set.
 		hs.add(e1);
 		hs.add(e2);
 
 		for(Employee emp : hs) {
-			System.out.println(emp);
+			System.out.println(emp); 
 		}
+
+		System.out.println("==============================");
+
+		//Creating another HashSet to store Integer values.
+		HashSet<Integer> hs2 = new HashSet<Integer>();
 		
+		hs2.add(10);
+		hs2.add(50);
+		hs2.add(20);
+		hs2.add(40);
+		hs2.add(10); //duplicate -> ignored.
+		hs2.add(30);
+		hs2.add(20); //duplicate -> ignored.
+		
+		hs2.add(null); //allowed
+		hs2.add(null); //duplicate -> ignored.
+		
+		for(Integer i : hs2) {
+			System.out.println(i);
+		}
 		
 	}
 
