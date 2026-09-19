31c084a33fedad6cb3285ab7bbb05cad3b17e1f1
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-19 12:48:18 +0530

    Update HashSetDemo.java

--- Eclipse programs/Collections/Set/HashSetDemo.java
+++ Eclipse programs/Collections/Set/HashSetDemo.java
@@ -1,18 +1,20 @@
 package sets;
-import java.util.HashSet;
+import java.util.HashSet;  //HashSet class imported to use it.
 
 public class HashSetDemo {
 
 	public static void main(String[] args) {
-		
+		//Creating a HashSet
 		HashSet<Employee> hs = new HashSet<Employee>();
-		
+
+		//Creating Employee class objects to invoke the constructor and intialize the variables. 
 		Employee e1 = new Employee(101, 24, "Eren Yeagr", 12000);
 		Employee e2 = new Employee(102, 25, "Levi Ackerman", 17000);
 		Employee e3 = new Employee(103, 22, "Arvin Smith", 20000);
 		Employee e4 = new Employee(104, 21, "Hange Zoe", 15000);
 		Employee e5 = new Employee(105, 25, "Mikasa Ackerman", 14000);
-		
+
+		//Adding the objects to the HashSet collection.
 		hs.add(e1);
 		hs.add(e2);
 		hs.add(e3);
