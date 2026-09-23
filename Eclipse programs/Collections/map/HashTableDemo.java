b126b842ac030a31296a61a79513fc08871d210b
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-23 22:07:20 +0530

    Update HashTableDemo.java

--- Eclipse programs/Collections/map/HashTableDemo.java
+++ Eclipse programs/Collections/map/HashTableDemo.java
@@ -1,15 +1,15 @@
 package map;
 
-import java.util.Hashtable;
-import java.util.Set;
+import java.util.Hashtable; //importing Hashtable class to use it.
+import java.util.Set;  //importing Set to convert and store the keys as Set.
 
 public class HashTableDemo {
 
 	public static void main(String[] args) {
-		
+		//Creating a Hashtable with Integer type keys and String type values.
 		Hashtable<Integer, String> ht = new Hashtable<Integer, String>();
-		
-		//Hashtable does not allow any null key or value.
+		//Adding key value pairs.
+		//Hashtable does not allow any null keys or values.
 		ht.put(10, "Crysta");
 		ht.put(9, "Connie");
 		ht.put(7, "jean");
@@ -24,9 +24,11 @@ public class HashTableDemo {
 		//Duplicate values are allowed but not keys.
 		ht.put(11, "Sasha");
 		ht.put(12, "Ymir");
-		
+
+		//Converting the Keys to a set.
 		Set<Integer> keyCadets = ht.keySet();
-		
+
+		//Traversing through the Set using for each loop.
 		for(Integer Scout : keyCadets) {
 			System.out.println(ht.get(Scout)+" is on number "+Scout);
 		}
