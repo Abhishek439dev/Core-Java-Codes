11367a4585622013374bae63c00135674d66cf93
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-23 20:29:41 +0530

    Update HashMapDemo.java

--- Eclipse programs/Collections/map/HashMapDemo.java
+++ Eclipse programs/Collections/map/HashMapDemo.java
@@ -1,14 +1,15 @@
 package map;
 
-import java.util.HashMap;
-import java.util.Set;
+import java.util.HashMap; //importing HashMap class to use it.
+import java.util.Set; //importing Set to convert and store the Keys as a Set.
 
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		
+		//Creating a HashMap with Keys as Integers and Values as String.
 		HashMap<Integer, String> hs  = new HashMap<Integer, String>();
-		
+
+		//Adding keys and Values to the HashMap.
 		hs.put(102, "Mikasa");
 		hs.put(105, "Erwin");
 		hs.put(104, "Levi");
@@ -24,21 +25,25 @@ public class HashMapDemo {
 		hs.put(107, "Eren"); 
 		hs.put(108, "Armin");
 		
-		//HashMap allows one null Key and multiple null Values.
+		//HashMap allows one null Key(as duplicate keys are not allowed) and multiple null Values(duplicate values allowed).
 		hs.put(109, null);
 		hs.put(null, null);
 		hs.put(110, "Connie");
-		
+
+		//Creating a set of the keys present in the HashMap to traverse through it.
 		Set<Integer> hsKeys = hs.keySet();
-		
+
+		//traversing through the Set of keys using the for-each loop.
 		for(Integer keys : hsKeys) {
 			System.out.println("Cadet with Id "+keys+" is "+hs.get(keys));
 		}
 		
 		System.out.println("========================");
-		
+
+		//Creating another HashMap with String keys and Integer values.
 		HashMap<String, Integer> hs2  = new HashMap<String, Integer>();
-		
+
+		//adding key value pairs.
 		hs2.put("Apple", 250);
 		hs2.put("Banana", 60);
 		hs2.put("Mango", 70);
@@ -48,9 +53,11 @@ public class HashMapDemo {
 		//Duplicate keys with different values -> Override the previous values.
 		hs2.put("Mango", 50);
 		hs2.put("Pomegranate", 250);
-		
+
+		//Creating Set of the keys.
 		Set<String> hsKeys2 = hs2.keySet();
-		
+
+		//Traversing through the set.
 		for(String keys : hsKeys2) {
 			System.out.println(keys+" is "+hs2.get(keys)+" ruppees per kg.");
 		}
