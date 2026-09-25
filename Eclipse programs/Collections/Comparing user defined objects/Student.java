d538f1e7ca7d89214b5967303c6b08e38851526d
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-09-25 23:44:37 +0530

    Update Student.java

--- Eclipse programs/Collections/Comparing user defined objects/Student.java
+++ Eclipse programs/Collections/Comparing user defined objects/Student.java
@@ -1,11 +1,15 @@
 package comparing_objects;
-
+//To compare user-defined objects we need to override the compareTo method in the class,
+//whose objects have to be compared. 
+//For detault sorting we need to import the Comparable interface and Override the compareTo method.
 public class Student implements Comparable<Student>{
 
+	//declaring some variables to initialise via constructor.
 	int age;
 	int roll;
 	String name;
-	
+
+	//Creating a constructor to initialise the variables.
 	public Student(int age, int roll, String name) {
 		
 		this.age = age;
@@ -13,11 +17,13 @@ public class Student implements Comparable<Student>{
 		this.name = name;
 	}
 
+	//Overriding the toString method so that we get the actual content instead of Strung representation.
 	@Override
 	public String toString() {
 		return "Student [age=" + age + ", roll=" + roll + ", name=" + name + "]";
 	}
 	
+	//Overriding the compareTo method with customised logic to compare and sort objectd of Student class 
 	@Override
 	public int compareTo(Student s) {
 		return this.age - s.age;
